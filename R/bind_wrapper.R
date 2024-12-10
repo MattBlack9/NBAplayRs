ecc83415f817bceb678dd6d2b368b28574024986
Matthew Blackley <matt9black@gmail.com>
2024-12-09 23:05:36 -0600

    Adding another function

---
+++ R/bind_wrapper.R
@@ -0,0 +1,45 @@
+library(lookup)
+library(tidyverse)
+library(rvest)
+library(lubridate)
+library(readxl)
+library(dplyr)
+library(readxl)
+library(aod)
+library(ggplot2)
+library(tidyr)
+library(stringr)
+library(lookup)
+library(reshape)
+library(coda)
+
+#' Title
+#'
+#' @param year dataframe of team
+#' @param time year
+#' @param team team and year
+#' @param reg_post regular or post season
+#'
+#' @return returns VORP of every player for a single team
+#' @export
+#'
+#' @examples
+#' data("reg_bos_80")
+#' 
+#' test <- reg.bos.80[[1]][1,]
+#' vor <- vorp(test)
+#' 
+#' individual(vor, 1985, 'BOS-1985', 'regular')
+
+
+individual <- function(year, time, team, reg_post){
+  df <- filter(year, season_id==team)
+  new <- data.frame()
+  for(i in unique(df$player)){
+    per <- filter(df,player==i)
+    score <- mean(per$VORP)
+    adder <- data.frame(player = i, TVORP = score, year = time, time = reg_post)
+    new <- rbind(new,adder)
+  }
+  return(new)
+}
